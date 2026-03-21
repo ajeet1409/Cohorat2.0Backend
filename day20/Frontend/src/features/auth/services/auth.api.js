720a5af056e70274066b5c111157269b3d82080a
aj <sahaniajeet14@gmail.com>
2026-03-22 05:28:01 +0530

    url chanage to localhost to render in frontend for deployment

--- day20/Frontend/src/features/auth/services/auth.api.js
+++ day20/Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api || https://cohorat2-0backend.onrender.com/api",
+  baseURL: "https://cohorat2-0backend.onrender.com/api",
   withCredentials: true,
 });
 
--- day20/Frontend/src/features/post/services/post.api.js
+++ day20/Frontend/src/features/post/services/post.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000/api || https://cohorat2-0backend.onrender.com/api",
+  baseURL: "https://cohorat2-0backend.onrender.com/api",
   withCredentials: true,
 });
 
